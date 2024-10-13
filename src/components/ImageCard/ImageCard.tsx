9a9a8bd015e40948f14431805111c87f974d35b8
sydorovay <sydorovay@gmail.com>
2024-10-14 00:41:37 +0200

    Update ImageCard.tsx

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -1,8 +1,15 @@
+import React from 'react';
 import css from './ImageCard.module.css';
+import { ImageType } from '../types/types';
 
-const ImageCard = ({ image, onImageClick }) => (
-  <div onClick={() => onImageClick(image)}>
-    <img className={css.imageCardImg} src={image.urls.small} alt={image.alt_description} />
+interface ImageCardProps {
+  image: ImageType;
+  onImageClick: (image: ImageType) => void;
+}
+
+const ImageCard: React.FC<ImageCardProps> = ({ image, onImageClick }) => (
+  <div className={css.imageCard} onClick={() => onImageClick(image)}>
+    <img src={image.urls.small} alt={image.alt_description} className={css.image} />
   </div>
 );
 
