392c457624e1ae45cb93462c1379b443dad6816f
sydorovay <sydorovay@gmail.com>
2024-10-14 01:42:23 +0200

    Update ImageModal.tsx

--- src/components/ImageModal/ImageModal.tsx
+++ src/components/ImageModal/ImageModal.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import Modal from 'react-modal';
+import ReactModal from 'react-modal';
 import css from './ImageModal.module.css';
 import { ImageType } from '../types/types';
 
@@ -10,24 +9,21 @@ interface ImageModalProps {
 
 const ImageModal: React.FC<ImageModalProps> = ({ image, onClose }) => {
   if (!image) return null;
-
   return (
-    <Modal 
-      isOpen={true} 
+    <ReactModal
+   isOpen={true} 
       onRequestClose={onClose} 
       contentLabel="Image Modal"
       ariaHideApp={false}
-      className={css.modal} 
+      className={css.modal}
       overlayClassName={css.overlay}
       shouldCloseOnOverlayClick={true}
       closeTimeoutMS={200}
     >
-      <div>
-        <h2>{image.alt_description || 'Image'}</h2>
-        <img src={image.urls.regular} alt={image.alt_description || 'Image'} />
-        <button onClick={onClose}>Close</button>
-      </div>
-    </Modal>
+      <h2>{image.alt_description || 'Image'}</h2>
+      <img src={image.urls.regular} alt={image.alt_description || 'Image'} />
+      <button onClick={onClose}>Close</button>
+    </ReactModal>
   );
 };
 
